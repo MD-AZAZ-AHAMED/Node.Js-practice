ffe163e1bb0e89e930849a473617dedea298ce18
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2025-12-16 20:51:57 +0600

    Add files via upload

---
+++ Lecture1/index.js
@@ -0,0 +1,7 @@
+console.log('Qwick Start');
+
+console.log(4**10);
+
+console.log(23**10);
+
+console.log('nodemon inbstalled successfully');
---
+++ Lecture2/index.js
@@ -0,0 +1,4 @@
+
+//const {sum, sub, mul, div} = require('./utils');
+import {sum, sub, mul, div} from './utils.js';
+console.log("Sum: ", sum(10, 5));
