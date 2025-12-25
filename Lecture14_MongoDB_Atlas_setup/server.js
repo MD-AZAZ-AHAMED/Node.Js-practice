3347f35b4653d3789960a073922340148f93709e
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2025-12-26 03:44:46 +0600

    Add files via upload

---
+++ Lecture14_MongoDB_Atlas_setup/server.js
@@ -0,0 +1,21 @@
+// USer Name MongoDB Atlas Project : mdazazahamed_db_user
+// Password:  4iMbQna4fMhWQV99
+
+
+
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+mongoose
+  .connect('mongodb+srv://mdazazahamed_db_user:4iMbQna4fMhWQV99@cluster0.yhtd3vc.mongodb.net/', {
+    dbName: 'nodejs_mastery_course',
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+const port = 3000;
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
