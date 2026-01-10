c055be3267535cb5a4ff1c181392e5a180fd8ad3
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2026-01-10 23:01:12 +0600

    Add files via upload

---
+++ Lecture1/taking multiple input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"please enter 2 numbers :";
+	cin>>num1>>num2;
+	int sum;
+	sum=num1+num2;
+	cout<<"summation of both number is :"<<sum;
+	getch();
+}
