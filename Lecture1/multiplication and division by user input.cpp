35db58ed683d0478f782817a8ff0242a388c82b4
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2026-01-13 03:43:37 +0600

    Add files via upload

---
+++ Lecture1/multiplication and division by user input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"please enter 2 numbers :";
+	cin>>num1>>num2;
+	int division;
+	division=num1/num2;
+	cout<<"division is : "<<division<<endl;
+	int multiplication;
+	multiplication=num1*num2;
+	cout<<"multiplication is : "<<multiplication<<endl;
+	getch();
+	
+}
