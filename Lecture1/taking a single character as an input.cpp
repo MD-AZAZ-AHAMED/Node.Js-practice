10431d3423910754b2d5a563babb1aca4edf783f
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2026-01-15 15:37:05 +0600

    Add files via upload

---
+++ Lecture1/taking a single character as an input.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char character;
+    cout<<"Please enter a character :";
+    cin>>character;
+    cout<<"your entered chharacter is :"<<character;
+    getch();
+}
---
+++ Lecture1/taking a string as an input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	char bara[8];
+	cout<<"please enter your bara khor :";
+	cin>>bara;
+	cout<<"your entered bitch is :"<<bara;
+	getch();
+}
