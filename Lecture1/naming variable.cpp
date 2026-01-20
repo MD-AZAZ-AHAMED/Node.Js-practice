922dd337978383c66ecfa8f8ce7c58fbc2af229a
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2026-01-20 21:50:05 +0600

    Add files via upload

---
+++ Lecture1/naming variable.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int num1=998,num2=876,num3=987;
+    cout<<"Number 1="<<num1<<endl<<"number 2="<<num2<<endl<<"number 3="<<num3;
+    getch();
+}
---
+++ Lecture1/setprecision.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	float num1,num2;
+	cout<<"please enter 2 numbers :";
+	cin>>num1>>num2;
+	cout<<showpoint;
+	cout<<setprecision(11);
+	float sum=num1+num2;
+	cout<<"sum is :"<<sum<<endl;
+	float mul=num1*num2;
+	cout<<"multiplication is :"<<mul<<endl;
+	float division=num1/num2;
+	cout<<"division is :"<<division<<endl;
+	getch();
+	
+}
