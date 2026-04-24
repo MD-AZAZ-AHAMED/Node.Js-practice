665ba85956db5358e06e96b481fb80f67672b7e0
MD. AZAZ AHAMED <68296908+MD-AZAZ-AHAMED@users.noreply.github.com>
2026-04-24 11:10:51 +0600

    Add files via upload

--- Lecture1/server.js
+++ Lecture1/server.js
@@ -1,7 +1,6 @@
-const app=require("./src/app")
-const connectDB=require("./src/db/db")
+const app= require('./src/app');
 
-connectDB()
-app.listen(3000,() =>{
-    console.log("server is running on port 3000")
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
 })
